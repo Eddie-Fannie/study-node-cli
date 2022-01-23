14f3c5d8f9af22b0384f33cd39ccf22b2fae7ffc
Jiaheng Lin <jiaheng.lin@shopee.com>
2022-01-23 19:03:24 +0800

    feat: upgrade function and add tsup for ts

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,19 @@
 
 // 请求 commander 库
 const program = require('commander');
+const updateChk = require('../dist/update');
 
 // 从 package.json 文件中请求version 字段的值， -v 和 -- version 都有效
 const packageInfo = require('../package.json');
 program.version(packageInfo.version, '-v, --version');
 
-console.log(process.argv);
+// upgrade
+program
+  .command('upgrade')
+  .description(`Check the ${packageInfo.name} version.`)
+  .action(() => {
+    updateChk();
+  });
 
 // 解析命令行参数
 program.parse(process.argv);
